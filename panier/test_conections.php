0a0aeb048f869586473158d331863656a51140d6
Sam <153606332+Sammixi@users.noreply.github.com>
2024-06-11 19:05:03 +0200

    dcdvzv

--- panier/test_conections.php
+++ panier/test_conections.php
@@ -15,8 +15,7 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Vérifier la connexion
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: (" . $conn->connect_errno . ") " . $conn->connect_error);
 } else {
     echo "Connection successful!";
 }
-?>
