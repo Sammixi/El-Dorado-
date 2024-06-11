fd4ea38ef54fc5f88b0f6220abe891ee6b1869a1
Sam <153606332+Sammixi@users.noreply.github.com>
2024-06-11 19:13:04 +0200

    ezeze

--- test_conections.php
+++ test_conections.php
@@ -4,10 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Remplacez ces valeurs par vos informations MySQL Hostinger
-$servername = "mysql.hostinger.com"; // Nom d'hôte MySQL de Hostinger
+$servername = "mysql.hostinger.com";
 $port = "3306"; // Généralement, le port est 3306
 $username = "u652266785_ElDorado"; // Votre nom d'utilisateur MySQL
-$password = "5_9V69ysxAC5d#u"; // Votre mot de passe MySQL
+$password = "5_9V69yxsAC5d#u"; // Votre mot de passe MySQL
 $dbname = "u652266785_db_eldorado"; // Le nom de votre base de données
 
 // Créer la connexion
